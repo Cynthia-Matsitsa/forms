c831475c4e36a89465bc8166e507f58cb81f1e64
Cynthia-Matsitsa <cynthiamatsitsa@gmail.com>
2024-02-21 08:01:12 +0300

    Add file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+fun main() {
+    var x=modulus(6,80)
+    println(x)
+printName("Cynthia")
+     var y=add(4,5,6,10)
+    println(y)
+    var word = "Kind"
+    println(word)
+
+}
+fun printName(name:String){
+    println("Hello"+ "" +name)
+
+
+
+
+}
+     fun modulus(num1: Int ,num2: Int):Int{
+         var results = num1 % num2
+         return results
+
+     }
+
+
+     fun add(num1: Int ,num2: Int , num3: Int , num4:Int):Int{
+         var results = num1 + num2 + num3 + num4
+         return results
+     }
+
+fun fact(word: String){
+    println("word")
+
+
+     }
